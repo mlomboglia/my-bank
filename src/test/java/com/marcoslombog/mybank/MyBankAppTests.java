7722723dd715d8cadf8e443b46a7f4050e5706d3
mlomboglia <marcos.lomboglia@gmail.com>
2019-03-15 12:08:24 +0000

    Included tests

--- src/test/java/com/marcoslombog/mybank/MyBankAppTests.java
+++ src/test/java/com/marcoslombog/mybank/MyBankAppTests.java
@@ -1,16 +1,67 @@
 package com.marcoslombog.mybank;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.marcoslombog.mybank.model.Account;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = App.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MyBankAppTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+	@LocalServerPort
+	private int port;
+
+	private String getRootUrl() {
+		return "http://localhost:" + port;
+	}
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testGetAllAccounts() {
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+		ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/accounts/all", HttpMethod.GET, entity,
+				String.class);
+		Assert.assertNotNull(response.getBody());
+	}
+
+	@Test
+	public void testGetAccountById() {
+		Account account = restTemplate.getForObject(getRootUrl() + "/accounts/1", Account.class);
+		Assert.assertNotNull(account);
+	}
+
+	@Test
+	public void testCreateAccount() {
+		Account account = new Account();
+		account.setName("Paul Roberts");
+		account.setBalance(new Double(500));
+		ResponseEntity<Account> postResponse = restTemplate.postForEntity(getRootUrl() + "/accounts/new", account, Account.class);
+		Assert.assertNotNull(postResponse);
+		Assert.assertNotNull(postResponse.getBody());
+	}
+
+	@Test
+	public void testUpdateAmount() {
+		int id = 1;
+		restTemplate.put(getRootUrl() + "/accounts/" + id, new Double(100));
+		Account updatedAccount = restTemplate.getForObject(getRootUrl() + "/accounts/" + id, Account.class);
+		Assert.assertNotNull(updatedAccount);
+	}
 }
